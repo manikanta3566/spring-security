61531baa9c90891d98966480f9efd66a5e44d1b1
Manikanta M <manikantapoojary444@gmail.com>
2022-05-12 20:53:14 -0700

    first commit

---
+++ spring-security-using-basic-auth/src/main/java/com/practice/springsecurityusingbasicauth/SpringSecurityUsingBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springsecurityusingbasicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityUsingBasicAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityUsingBasicAuthApplication.class, args);
+	}
+
+}
